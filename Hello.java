f6f0c899d8c46ff4443da584701bf0187b438145
Krishnan <krishnan@vigshakri.com>
2019-09-20 14:54:58 -0700

    Java Demo

---
+++ Hello.java
@@ -0,0 +1,9 @@
+class Hello{  
+    public static void main(String[] args){  
+		System.out.println("1 This is java app \n by using Docker"); 
+		System.out.println("2 This is java app \n by using Docker"); 
+		System.out.println("3 This is java app \n by using Docker"); 
+		System.out.println("4 This is java app \n by using Docker"); 
+		System.out.println("5 This is java app \n by using Docker"); 
+    }  
+}  
